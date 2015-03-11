4a81d628e4ca28b3ea25c74b333c4540ced248c2
gileze33 <giles.williams@gmail.com>
2015-03-11 14:17:27 +0000

    Use ES 1.4.4 instead of 1.3.4

--- elasticsearch-ec2.sh
+++ elasticsearch-ec2.sh
@@ -4,29 +4,29 @@
 
 
 if [ "$(whoami)" != "root" ]; then
-	echo "Sorry, you are not root."
-	exit 1
+    echo "Sorry, you are not root."
+    exit 1
 fi
 
-wget http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.3.4.zip
-unzip elasticsearch-1.3.4 -d /usr/local/elasticsearch
-rm elasticsearch-1.3.4.zip
+wget http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.4.4.zip
+unzip elasticsearch-1.4.4 -d /usr/local/elasticsearch
+rm elasticsearch-1.4.4.zip
 
-cd /usr/local/elasticsearch/elasticsearch-1.3.4/
+cd /usr/local/elasticsearch/elasticsearch-1.4.4/
 bin/plugin -install elasticsearch/elasticsearch-cloud-aws/2.2.0
 
-echo "cluster.name: $0" >> /usr/local/elasticsearch/elasticsearch-1.3.4/config/elasticsearch.yml
-echo "cloud:" >> /usr/local/elasticsearch/elasticsearch-1.3.4/config/elasticsearch.yml
-echo "    aws:" >> /usr/local/elasticsearch/elasticsearch-1.3.4/config/elasticsearch.yml
-echo "        access_key: $1" >> /usr/local/elasticsearch/elasticsearch-1.3.4/config/elasticsearch.yml
-echo "        secret_key: $2" >> /usr/local/elasticsearch/elasticsearch-1.3.4/config/elasticsearch.yml
-echo "        region: $4" >> /usr/local/elasticsearch/elasticsearch-1.3.4/config/elasticsearch.yml
-echo "discovery:" >> /usr/local/elasticsearch/elasticsearch-1.3.4/config/elasticsearch.yml
-echo "    type: ec2" >> /usr/local/elasticsearch/elasticsearch-1.3.4/config/elasticsearch.yml
-echo "repositories:" >> /usr/local/elasticsearch/elasticsearch-1.3.4/config/elasticsearch.yml
-echo "    s3:" >> /usr/local/elasticsearch/elasticsearch-1.3.4/config/elasticsearch.yml
-echo "        bucket: $3" >> /usr/local/elasticsearch/elasticsearch-1.3.4/config/elasticsearch.yml
-echo "        region: $4" >> /usr/local/elasticsearch/elasticsearch-1.3.4/config/elasticsearch.yml
+echo "cluster.name: $0" >> /usr/local/elasticsearch/elasticsearch-1.4.4/config/elasticsearch.yml
+echo "cloud:" >> /usr/local/elasticsearch/elasticsearch-1.4.4/config/elasticsearch.yml
+echo "    aws:" >> /usr/local/elasticsearch/elasticsearch-1.4.4/config/elasticsearch.yml
+echo "        access_key: $1" >> /usr/local/elasticsearch/elasticsearch-1.4.4/config/elasticsearch.yml
+echo "        secret_key: $2" >> /usr/local/elasticsearch/elasticsearch-1.4.4/config/elasticsearch.yml
+echo "        region: $4" >> /usr/local/elasticsearch/elasticsearch-1.4.4/config/elasticsearch.yml
+echo "discovery:" >> /usr/local/elasticsearch/elasticsearch-1.4.4/config/elasticsearch.yml
+echo "    type: ec2" >> /usr/local/elasticsearch/elasticsearch-1.4.4/config/elasticsearch.yml
+echo "repositories:" >> /usr/local/elasticsearch/elasticsearch-1.4.4/config/elasticsearch.yml
+echo "    s3:" >> /usr/local/elasticsearch/elasticsearch-1.4.4/config/elasticsearch.yml
+echo "        bucket: $3" >> /usr/local/elasticsearch/elasticsearch-1.4.4/config/elasticsearch.yml
+echo "        region: $4" >> /usr/local/elasticsearch/elasticsearch-1.4.4/config/elasticsearch.yml
 
 
 
@@ -35,6 +35,6 @@ cd bin
 yum install -y git
 git clone https://github.com/elasticsearch/elasticsearch-servicewrapper.git
 mv elasticsearch-servicewrapper/service ./
-/usr/local/elasticsearch/elasticsearch-1.3.4/bin/service/elasticsearch install
+/usr/local/elasticsearch/elasticsearch-1.4.4/bin/service/elasticsearch install
 sudo chkconfig elasticsearch on
 shutdown -r now
